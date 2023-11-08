b2c306063ebc49844bde393a09c452db8b2457b2
Square bits <squarebits@Squares-Mac-mini.local>
2023-11-08 19:09:57 +0530

    updation in api

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import toast from 'react-hot-toast';
 
 function App() {
   const [totalScore, setTotalScore] = useState('')
-  const [totalBall, setTotalBall] = useState("")
+  const [totalBall, setTotalBall] = useState(0)
   const [yourScore, setYourScore] = useState(0)
   const [needToWin, setNeedToWin] = useState(0)
   const [show, setShow] = useState(true)
   const [time, setTime] = useState(5)
   const [playing, setPlaying] = useState(false)
   const [opponentScoreGot, setOpponentScoreGot] = useState(false)
-
+  const [runs, setRuns] = useState([])
   const getOpponentScore = () => {
     const TotalScr = Math.round(Math.random() * 36)
     setTotalScore(TotalScr)
@@ -30,6 +30,7 @@ function App() {
       setTimeout(() => {
         setShow(true);
         setOpponentScoreGot(false)
+        setRuns([]);
         setNeedToWin(0);
         setYourScore(0);
         setTotalBall(0);
@@ -66,8 +67,12 @@ function App() {
     setTimeout(() => {
       setPlaying(false)
       let runHitOnBall = Math.round(Math.random() * 6)
-      const updatedRunToWin = needToWin - runHitOnBall
-      const remainingBall = totalBall - 1
+      const updatedRunToWin = needToWin - (runHitOnBall === 5 ? 1 : runHitOnBall)
+      let remainingBall;
+      // if run is 5 then consider it as wide
+      if (runHitOnBall !== 5) remainingBall = totalBall - 1
+      else remainingBall = totalBall
+
       if (remainingBall > 0)
         setTotalBall(remainingBall)
       else setTotalBall(0)
@@ -75,32 +80,44 @@ function App() {
       if (updatedRunToWin > 0)
         setNeedToWin(updatedRunToWin)
       else if (updatedRunToWin <= 0) setNeedToWin(0)
-      setYourScore(yourScore + runHitOnBall)
+      runs.push(runHitOnBall === 5 ? 'Wd' : runHitOnBall)
+      setRuns(runs)
+      setYourScore(yourScore + (runHitOnBall === 5 ? 1 : runHitOnBall))
     }, 2000);
 
   }
-  const lastBall = [0, 5, "W", 8]
   return (
 
     <div className='Main-Box'>
       <div className='Score-Card'>
         {
-          show ? <>
-            {opponentScoreGot ? <h2>OPPONENT SCORE : {totalScore}</h2> : ''}
+          <>
+            {opponentScoreGot ?
+              <h2 className='Change-color'>OPPONENT SCORE : {totalScore}</h2> :
+              <h2 className='Change-color'>GET OPPONENT SCORE</h2>}
             <h2>NEED {needToWin} RUN IN {totalBall} BALL </h2>
             <h2>YOUR SCORE : {yourScore} </h2>
-            <div className='Last-Ball-Played' >  {lastBall.map((el) => <h1>{el}</h1>)}</div>
-          </> : <h1>Please Wait {time}...</h1>
+            {opponentScoreGot ? <div className='Last-Ball-Played' >
+              <div>
+                {runs.map((el) => <h1>{el}</h1>)}
+              </div>
+            </div> : ''}
+          </>
         }
       </div>
 
-      <div className='Play-Buttons'>
-        {
-          !opponentScoreGot ?
-            <button className='Opponent-Button' onClick={getOpponentScore}>OPPONENT SCORE</button> :
-            <button className='Play-Ball-Button' onClick={playBall}> {playing ? "PLAYING..." : "PLAY BALL"}</button>
-        }
-      </div>
+      {
+        <div className='Play-Buttons'>
+          {show ?
+            !opponentScoreGot ?
+              <button className='Opponent-Button' onClick={getOpponentScore}>OPPONENT SCORE</button> :
+              playing ? <button className='Play-Ball-Button disabledBts' disabled> PLAYING...</button> :
+                <button className='Play-Ball-Button' onClick={playBall} > PLAY </button>
+            :
+            <div className='Wait_Box'>  <h2>Please Wait {time}...</h2></div>
+          }
+        </div>
+      }
 
     </div>
   );
